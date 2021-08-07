0a6be322048739874a021227f5d8d272d652e50f
Craig <84283410+cshields1@users.noreply.github.com>
2021-08-07 08:05:17 -0700

    Set up project files

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1>Calculator</h1>
+<p>Basic arithmetic calculator built with HTML, JS, and Sass for The Odin Project.</p>
---
+++ scripts.js
@@ -0,0 +1,6 @@
+// add
+// subtract
+// multiply
+// divide
+
+// operate: takes an operator and 2 numbers and calls an above function
