64427c43654a6048331e5c23c5c13261c0f0917e
Craig <84283410+cshields1@users.noreply.github.com>
2021-08-10 16:43:23 -0700

    Fix logic

--- scripts.js
+++ scripts.js
@@ -1,58 +1,41 @@
-const btn1 = document.querySelector("#one");
-const btn2 = document.querySelector("#two");
-const btn3 = document.querySelector("#three");
-const btn4 = document.querySelector("#four");
-const btn5 = document.querySelector("#five");
-const btn6 = document.querySelector("#six");
-const btn7 = document.querySelector("#seven");
-const btn8 = document.querySelector("#eight");
-const btn9 = document.querySelector("#nine");
-const btn0 = document.querySelector("#zero");
-const btnDecimal = document.querySelector("#decimal");
-const btnSign = document.querySelector("#sign");
-
-const btnAdd = document.querySelector("#add");
-const btnSubtract = document.querySelector("#subtract");
-const btnMultiply = document.querySelector("#multiply");
-const btnDivide = document.querySelector("#divide");
-const btnEquals = document.querySelector("#equals");
-
-const btnBackspace = document.querySelector("#backspace");
-const btnClear = document.querySelector("#clear");
-
 const keypadBtns = document.querySelectorAll(".keypad");
+const decimalBtn = document.querySelector("#decimal");
 const operatorBtns = document.querySelectorAll(".operator");
+const signBtn = document.querySelector("#sign");
+const addBtn = document.querySelector("#add");
+const subtractBtn = document.querySelector("#subtract");
+const multiplyBtn = document.querySelector("#multiply");
+const divideBtn = document.querySelector("#divide");
+const equalsBtn = document.querySelector("#equals");
+const backspaceBtn = document.querySelector("#backspace");
+const clearBtn = document.querySelector("#clear");
 
 const display = document.querySelector("#display");
 display.textContent = "";
 
-for (let button of keypadBtns) {
-  button.value = button.textContent;
-}
+addBtn.operator = (a, b) => a + b;
+subtractBtn.operator = (a, b) => a - b;
+multiplyBtn.operator = (a, b) => a * b;
+divideBtn.operator = (a, b) =>
+  b === 0 ? "Division by zero is forbidden!" : a / b;
+
 let a = null;
 let b = null;
 let operator = null;
-let isRunning = false;
+let shouldClearDisplay = false;
 
-btnAdd.operator = (a, b) => a + b;
-btnSubtract.operator = (a, b) => a - b;
-btnMultiply.operator = (a, b) => a * b;
-btnDivide.operator = (a, b) =>
-  b === 0 ? "Division by zero is forbidden!" : a / b;
-
-// populate the display with numbers as they're pressed and store in display value
-for (let button of keypadBtns) {
-  button.addEventListener("click", function updateDisplayValue() {
-    if (a && !isRunning) {
-      a = 0;
+keypadBtns.forEach((button) => {
+  button.value = button.textContent;
+  button.addEventListener("click", function () {
+    if (shouldClearDisplay) {
       display.textContent = "";
-      isRunning = true;
+      shouldClearDisplay = false;
     }
     display.textContent += this.value;
   });
-}
+});
 
-btnSign.addEventListener("click", function changeSign() {
+signBtn.addEventListener("click", () => {
   if (!display.textContent.includes("-")) {
     display.textContent = "-" + display.textContent;
   } else {
@@ -60,65 +43,48 @@ btnSign.addEventListener("click", function changeSign() {
   }
 });
 
-// what happens when an operator is pressed instead of equals?
-// - operate
-// - returned value gets stored as "a"
-// - "b" is reset
-
-for (let button of operatorBtns) {
-  button.addEventListener("click", function processData() {
+operatorBtns.forEach((button) =>
+  button.addEventListener("click", function () {
+    if (operator) evaluate();
+    a = Number(display.textContent) || 0;
     operator = this.operator;
-    if (!a) a = Number(display.textContent) || 0;
-    if (!isRunning) {
-      display.textContent = "";
-    } else {
-      display.textContent = operate();
-    }
-    isRunning = true;
-    if (!display.textContent.includes("."))
-      btnDecimal.removeAttribute("disabled");
-  });
-}
+    shouldClearDisplay = true;
+  })
+);
 
-function operate() {
-  display.textContent ? (b = Number(display.textContent)) : (b = 0);
-  display.textContent = operator(a, b);
-  a = Number(display.textContent);
-  b = null;
-}
+equalsBtn.addEventListener("click", evaluate);
 
-btnEquals.addEventListener("click", function handleEquals() {
-  if (operator) {
-    operate();
-    a = Number(display.textContent);
-    b = null;
-    operator = null;
-    isRunning = false;
-  }
-});
+decimalBtn.addEventListener("click", () =>
+  decimalBtn.setAttribute("disabled", "")
+);
 
-// only one decimal allowed
-btnDecimal.addEventListener("click", function addDecimal() {
-  btnDecimal.setAttribute("disabled", "");
-});
-// undo button
-btnBackspace.addEventListener("click", function deleteNumber() {
-  if (display.textContent) {
-    display.textContent = display.textContent.slice(
+backspaceBtn.addEventListener(
+  "click",
+  () =>
+    (display.textContent = display.textContent.slice(
       0,
       display.textContent.length - 1
-    );
-  }
-});
-// clear button should wipe out any existing data
-btnClear.addEventListener("click", function clearAll() {
+    ))
+);
+
+clearBtn.addEventListener("click", () => {
   a = null;
   b = null;
   operator = null;
   display.textContent = "";
-  isRunning = false;
-  btnDecimal.removeAttribute("disabled");
+  shouldClearDisplay = false;
+  decimalBtn.removeAttribute("disabled");
 });
+
+function evaluate() {
+  if (!display.textContent.includes("."))
+    decimalBtn.removeAttribute("disabled");
+  b = Number(display.textContent) || 0;
+  display.textContent = operator(a, b);
+  a = Number(display.textContent);
+  b = null;
+  shouldClearDisplay = true;
+}
 
 // evaluate only one pair of digits at a time
 
